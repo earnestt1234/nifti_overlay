793d0e366c63f1c181b7b7484c40f33dff72473e
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2024-11-14 22:41:21 -0600

    add tests for all major parameters of Anatomy

--- tests/test_anatomy.py
+++ tests/test_anatomy.py
@@ -8,6 +8,7 @@ Created on Tue Nov 12 20:20:28 2024
 
 import matplotlib
 import numpy as np
+from numpy import nan
 import pytest
 
 from nifti_overlay.image import Anatomy
@@ -20,6 +21,16 @@ def test_defaults(nifti_path):
     assert img.drop_zero == False
     assert img.vmin is None
     assert img.vmax is None
+
+def test_drop_zero(nifti_path):
+    img = Anatomy(nifti_path, drop_zero=True)
+    datax = img.get_slice(0, 3)
+    ansx = np.array([[nan, nan, nan, nan, nan, nan, nan],
+                     [nan, 1., 1., 1., 1., 1., nan],
+                     [nan, 1., 2., 2., 2., 1., nan],
+                     [nan, 1., 1., 1., 1., 1., nan],
+                     [nan, nan, nan, nan, nan, nan, nan]])
+    assert np.all(datax == ansx)
 
 def test_get_slice(nifti_path):
     img = Anatomy(nifti_path)
@@ -49,12 +60,6 @@ def test_get_slice(nifti_path):
     assert np.all(dataz == ansz)
 
 @pytest.mark.parametrize('dimension', [0, 1, 2])
-def test_get_slice_dropzero(nifti_path, dimension):
-    img = Anatomy(nifti_path, drop_zero=True)
-    data = img.get_slice(dimension, 3)
-    assert isinstance(data, np.ma.MaskedArray)
-
-@pytest.mark.parametrize('dimension', [0, 1, 2])
 @pytest.mark.parametrize('position', [0, 1, 2])
 def test_plot_slice(nifti_path, dimension, position):
     img = Anatomy(nifti_path)
@@ -63,35 +68,30 @@ def test_plot_slice(nifti_path, dimension, position):
     data_arr = img.get_slice(dimension, position)
     assert np.array_equal(plot_arr, data_arr)
 
-@pytest.mark.parametrize('color', ['gist_grey', 'jet'])
-@pytest.mark.parametrize('alpha', [1, .5])
-@pytest.mark.parametrize('scale_panel', [True, False])
-@pytest.mark.parametrize('drop_zero', [True, False])
-@pytest.mark.parametrize('vmin', [None, 0.5])
-@pytest.mark.parametrize('vmax', [None, 0.5])
-def test_plot_slice_against_init_parameters(nifti_path,
-                                            color,
-                                            alpha,
-                                            scale_panel,
-                                            drop_zero,
-                                            vmin,
-                                            vmax):
-    img = Anatomy(nifti_path,
-                  color=color,
-                  alpha=alpha,
-                  scale_panel=scale_panel,
-                  drop_zero=drop_zero,
-                  vmin=vmin,
-                  vmax=vmax)
-    dimension = 0
-    position = 0
-    ax = img.plot_slice(dimension, position)
-    plot_arr = ax.get_array()
-    data_arr = img.get_slice(dimension, position)
-    assert np.array_equal(plot_arr, data_arr, equal_nan=True)
-
 def test_plot_slice_returns_ax(nifti_path):
     img = Anatomy(nifti_path)
-    result = img.plot_slice(0, 0)
-    assert isinstance(result, matplotlib.image.AxesImage)
+    ax = img.plot_slice(0, 0)
+    assert isinstance(ax, matplotlib.image.AxesImage)
+
+def test_vmax(nifti_path):
+    vmax = 0.5
+    img = Anatomy(nifti_path, vmax=vmax)
+    ax = img.plot_slice(0, 0)
+    assert ax.get_clim()[1] == vmax
+
+def test_vmin(nifti_path):
+    vmin = 0.5
+    img = Anatomy(nifti_path, vmin=vmin)
+    ax = img.plot_slice(0, 0)
+    assert ax.get_clim()[0] == vmin
+
+def test_scale_panel(nifti_path):
+    dim, pos = 0, 0
+    img = Anatomy(nifti_path, scale_panel=True)
+    data = img.get_slice(dim, pos)
+    mini = np.nanmin(data)
+    maxi = np.nanmax(data)
+    ax = img.plot_slice(dim, pos)
+    assert ax.get_clim() == (mini, maxi)
+
 
